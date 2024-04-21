f83189b4aab6df2135bc680778b87e57d52e3b68
CRImier <crimier@yandex.ru>
2024-04-21 14:29:52 +0200

    Adding a tab closure feature to aggregate youtube tab script

--- aggregate_youtube_tabs.py
+++ aggregate_youtube_tabs.py
@@ -52,20 +52,23 @@ large_windows = {}
 def ask_if_music_tab(tab, leave_option=False):
     message = 'Is this music: \"{}\"?'.format(tab.name)
     if leave_option:
-        prompt = "[y]es? [N]o [l]eave? >>> "
+        prompt = "[y]es? [N]o [l]eave? [c]lose? >>> "
     else:
-        prompt = "[y]es? [N]o? >>> "
+        prompt = "[y]es? [N]o? [c]lose? >>> "
     print(message)
     result = None
-    valid_options = "YyNnLl" if leave_option else "YyNn"
+    valid_options = "YyNnLlCc" if leave_option else "YyNnCc"
     while not result or (result not in valid_options or len(result) > 1):
         result = input(prompt)
     if result.lower() == 'y':
         return True
     elif result.lower() == 'l':
-        return None
+        return "leave"
+    elif result.lower() == 'c':
+        return "close"
     return False
 
+tabs_to_close = []
 # here, we're asking the user to go through 5 tabs and mark them as either music or not music
 # if enough tabs in a window are music tabs, we determine the window to be a music window
 non_music_tabs = []
@@ -84,7 +87,9 @@ for window, count in youtube_windows.items():
             while tab in non_music_tabs:
                 tab = random.choice(common.filter_tabs_by_window(tabs, window))
             result = ask_if_music_tab(tab)
-            if result:
+            if result == "close":
+                tabs_to_close.append(tab)
+            elif result:
                 # we're not filtering music tabs from non-music windows yet
                 pass #music_tabs.append(tab)
             else:
@@ -95,6 +100,12 @@ for window, count in youtube_windows.items():
             music_window = window
         else:
             large_windows[window] = count
+
+for tab in tabs_to_close:
+    print("Closing tab {}".format(tab.get_full_id()))
+    cl.close_tabs([tab.get_full_id()])
+
+tabs_to_close = []
 
 if music_window is None:
     print("Music window not found! Sorting can't proceed")
@@ -150,7 +161,10 @@ for window in other_windows:
             continue
         result = ask_if_music_tab(tab, leave_option = True)
         do_leave = result is None
-        if do_leave:
+        if result == "leave":
+            continue
+        elif result == "close":
+            tabs_to_close.append(tab)
             continue
         elif result:
             destination = music_window
@@ -158,6 +172,10 @@ for window in other_windows:
             destination = largest_window
         print("Moving tab {} from window {} to window {}".format(tab.get_full_id(), tab.window, destination))
         tab.window = destination
+
+for tab in tabs_to_close:
+    print("Closing tab {}".format(tab.get_full_id()))
+    cl.close_tabs([tab.get_full_id()])
 
 new_tabs = common.serialize_tabs(tabs)
 common.update_tabs(cl, old_tabs, new_tabs)
