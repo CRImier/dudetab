93462b2100b05e0ff27a91c81c638b4a0187464a
CRImier <crimier@yandex.ru>
2025-08-15 23:16:22 +0200

    export_youtube_tabs: a whole lot of failproofing, and, teaching it to handle live/shorts URLs

--- export_youtube_tabs.py
+++ export_youtube_tabs.py
@@ -1,7 +1,8 @@
+import traceback
 import operator
 import random
-import re
 import json
+import re
 
 import common
 
@@ -11,6 +12,18 @@ out_file = "INSERT FILENAME HERE"
 
 cl = common.get_client()
 tabs = common.parse_tabs(cl)
+
+FunniException = type("FunniException", (Exception,), {})
+
+def get_url(url):
+    list_marker = "&list="
+    if "&list=" in url:
+        url = url.split(list_marker, 1)[0]
+    if "/shorts/" in url:
+        url = url.replace("/shorts/", "watch?v=")
+    if "/live/" in url:
+        url = url.replace("/live/", "watch?v=")
+    return url
 
 def ask_tab_action(tab):
     message = "Save tab?"
@@ -30,7 +43,6 @@ def ask_tab_action(tab):
     elif result.lower() == 'q':
         return "quit"
     else:
-        FunniException = type("FunniException", (Exception,), {})
         raise FunniException
 
 # closing YouTube Home tabs
@@ -41,7 +53,7 @@ for tab in youtube_home_tabs:
     print("Closing YouTube home tab in window {}".format(tab.window))
     cl.close_tabs([tab.get_full_id()])
 
-youtube_tabs = [tab for tab in tabs if "youtube.com/watch" in tab.url]
+youtube_tabs = [tab for tab in tabs if "youtube.com/watch" in get_url(tab.url)]
 
 tabs_to_close = []
 
@@ -52,50 +64,67 @@ for i, tab in enumerate(youtube_tabs):
     print("Downloading tab info {}/{}".format(i+1, len(youtube_tabs)))
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            info = ydl.extract_info(tab.url, download=False, process=False)
+            url = get_url(tab.url)
+            info = ydl.extract_info(url, download=False, process=False)
             sinfo = ydl.sanitize_info(info)
             tab.sinfo = sinfo
             duration = tab.sinfo.get("duration", 0)
+            if duration is None: duration = 0
             duration_m = duration // 60
             duration_s = duration % 60
             print(duration_m, duration_s)
     except yt_dlp.utils.DownloadError as e:
-        print(e)
+        traceback.print_exc()
+        tab.sinfo = {}
+    except Exception as e:
+        traceback.print_exc()
         tab.sinfo = {}
 
 # DURATION
 
 for i, tab in enumerate(youtube_tabs):
-    name = tab.name
-    pattern = re.compile("^\(\d*\)\s(.*)$")
-    match = pattern.match(name)
-    if match:
-        #print("pattern (NUMBER) matched!")
-        name = match.groups()[0]
-    end = ' - YouTube'
-    if name.endswith(end):
-        name = name[:-len(end)]
-    duration = tab.sinfo.get("duration", 0)
-    duration_m = duration // 60
-    duration_s = duration % 60
-    n = "{}/{}".format(i+1, len(youtube_tabs))
-    s = "{} - '{}' ({}:{}), from {}".format(tab.url, name, duration_m, duration_s, tab.sinfo.get("uploader", "[UNKNOWN]"))
-    print(n, s)
-    result = ask_tab_action(tab)
-    if result == "close":
-        tabs_to_close.append(tab)
-    elif result == "leave":
-        continue
-    elif result == "save":
-        with open(out_file, 'a') as f:
-            f.write(s+'\n')
-        tabs_to_close.append(tab)
-    elif result == "quit":
-        break
+    try:
+        name = tab.name
+        pattern = re.compile("^\(\d*\)\s(.*)$")
+        match = pattern.match(name)
+        if match:
+            #print("pattern (NUMBER) matched!")
+            name = match.groups()[0]
+        end = ' - YouTube'
+        if name.endswith(end):
+            name = name[:-len(end)]
+        duration = tab.sinfo.get("duration", 0)
+        if duration is None: duration = 0
+        duration_m = duration // 60
+        duration_s = duration % 60
+        n = "{}/{}".format(i+1, len(youtube_tabs))
+        url = get_url(tab.url)
+        s = "{} - '{}' ({}:{}), from {}".format(tab.url, name, duration_m, duration_s, tab.sinfo.get("uploader", "[UNKNOWN]"))
+        print(n, s)
+        result = ask_tab_action(tab)
+        if result == "close":
+            tabs_to_close.append(tab)
+        elif result == "leave":
+            continue
+        elif result == "save":
+            with open(out_file, 'a') as f:
+                f.write(s+'\n')
+            tabs_to_close.append(tab)
+        elif result == "quit":
+            break
+    except:
+        traceback.print_exc()
+
+cl = common.get_client()
+tabs = common.parse_tabs(cl)
+tab_ids = [tab.get_full_id() for tab in tabs]
 
 try:
     for tab in tabs_to_close:
-        print("Closing tab {}".format(tab.get_full_id()))
-        cl.close_tabs([tab.get_full_id()])
+        tab_id = tab.get_full_id()
+        if tab_id in tab_ids: # don't you dare trying and close the tab that's already been closed - that's either data loss, or a crash
+            print("Closing tab {}".format(tab_id))
+            cl.close_tabs([tab_id])
 except:
+    traceback.print_exc()
     print(tabs_to_close)
